f1cf312a8296e74fe81e15e6772749b9dd0c7bee
vlj2007 <vlj2007@yandex.ru>
2023-08-08 02:17:16 +0700

    I have corrected task 1 and I have finished task 2

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,24 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
     }
 
     public static void task1() {
+        System.out.println("Задача 1");
         String firstName = "Ivan";
         String middleName = "Ivanovich";
         String lastName = "Ivanov";
         String fullName = lastName + " " + firstName + " " + middleName;
-        System.out.println(fullName);
+        System.out.println("ФИО сотрудника — " + fullName + ".");
+    }
 
+    public static void task2() {
+        System.out.println("Задача 2");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName.toUpperCase() + ".");
     }
 }
