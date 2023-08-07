e70d33e03c89ff432ece2b3d79f7d81c583ad78c
vlj2007 <vlj2007@yandex.ru>
2023-08-08 01:36:02 +0700

    I have finished task1

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,11 @@ public class Main {
     }
 
     public static void task1() {
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println(fullName);
 
     }
 }
